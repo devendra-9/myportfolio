f6c67f75cf4cd2cc5dc4bcc77cf7b02bcc718cb5
Devendra <devendrakandpal07@gmail.com>
2024-07-03 10:11:39 +0530

    committed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import './css/global.css'
 
 import { HashLink as Hash } from 'react-router-hash-link'
 
 const navbar = () => {
   return (
-    <div className='Navbar'>
-      <div className="navbar-main">
-        <div className="left">
-          <h1>Portfolio</h1>
-        </div>
-        <div className="rightmenu">
-          <Hash to='#about' smooth='true'>About</Hash>
-          <Hash to='#project' smooth='true'>Project</Hash>
-          <Hash to='#skills' smooth='true'>Skills</Hash>
-          <Hash to='#contact' smooth='true'>Contact</Hash>
-        </div>
-      </div>  
+      <div className='navbar'>
+        <div className="navbar-main">
+          <div className="navbar-left">
+            <h1>Portfolio</h1>
+          </div>
+          <div className="navbar-right">
+            <ul>           
+                <Hash to='#about' smooth='true' style={{textDecoration:'none', fontsize:'18px',fontfamily:'Georgia',color: 'rgb(197, 192, 192)'}}><li>About</li></Hash>    
+                <Hash to='#project' smooth='true' style={{textDecoration:'none', fontsize:'18px',fontfamily:'Georgia',color: 'rgb(197, 192, 192)'}}><li>Project</li></Hash>
+                <Hash to='#skills' smooth='true' style={{textDecoration:'none', fontsize:'18px',fontfamily:'Georgia',color: 'rgb(197, 192, 192)'}}><li>Skills</li></Hash>
+                <Hash to='#contact' smooth='true' style={{textDecoration:'none', fontsize:'18px',fontfamily:'Georgia',color: 'rgb(197, 192, 192)'}}><li>Contact</li></Hash>
+            </ul>
+          </div>
+        </div>  
     </div>
   )
 }
